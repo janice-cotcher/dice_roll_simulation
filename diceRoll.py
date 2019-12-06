815bc64760c676013b3b60aa48c01a9910660cdb
Janice Cotcher <janice.cotcher@rbe.sk.ca>
2019-12-05 19:23:56 -0600

    update diceRoll.py
    added user input and exception handling

--- diceRoll.py
+++ diceRoll.py
@@ -1,3 +1,4 @@
+# demonstration of experimental probability with the rol of a single die
 # adapted from https://www.pythonforbeginners.com/code-snippets-source-code/game-rolling-the-dice/
 import random
 
@@ -6,15 +7,20 @@ min = 1
 max = 6
 n = 0
 rolls = []
-max_n = 10000
-while n <= max_n:
-    x = random.randint(min, max)
-    rolls.append(x)
-    n += 1
+while True:
+    max_n = int(input("How many times do you want to roll the die? "))
+    try:
+        while n <= max_n:
+            x = random.randint(min, max)
+            rolls.append(x)
+            n += 1
+        break
+    except ValueError:
+        print("Invalid choice, try again.")
 
 # count the number of times each number appears in n_max rolls append
 # and calculate the percent
-for y in range(1,7):
+for y in range(1, 7):
     print(f"The number of times {y+1} was rolled: {rolls.count(y)}")
     percent = rolls.count(y) * 100 / max_n
     print(f"The percent of times {y+1} was rolled: {percent}")
